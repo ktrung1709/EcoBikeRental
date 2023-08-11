3620c3080c67df19eda1c4d128a43bccb2ebe311
Hoang Duc Trung 20200640 <trung.hd200640@sis.hust.edu.vn>
2023-08-11 17:34:50 +0700

    fixing file name

--- 5. Programming/Ecobike/index.php
+++ 5. Programming/Ecobike/index.php
@@ -13,4 +13,15 @@ require_once 'model/bike/Bike.php'; // Include your model files as needed
 $initialDockListViewController = new InitialDockListViewController();
 $initialDockListViewController->displayInitialDockListView();
 
+// Handle requests based on user interaction
+$request = $_GET['request'];
 
+if ($request === 'bikesInDock') {
+    $dockId = $_GET['dockId'];
+    $bikesInDockViewController = new BikesInDockViewController();
+    $bikesInDockViewController->displayBikesInDockView($bikesInDock);
+} elseif ($request === 'bikeDetails') {
+    $bikeId = $_GET['bikeId'];
+    $bikeDetailsViewController = new BikeDetailsViewController();
+    $bikeDetailsViewController->displayBikeDetailsView($bike);
+}
