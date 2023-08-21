5c6b3dc5b505d96bc5b4fd9a17d6053a480c102e
ngmtea <107825873+ngmtea@users.noreply.github.com>
2023-08-21 18:02:56 +0700

    add returningDock to requesthandler

--- 5. Programming/Ecobike/requestHandler.php
+++ 5. Programming/Ecobike/requestHandler.php
@@ -5,6 +5,7 @@ require_once 'controller/BikesInDockViewController.php';
 require_once 'controller/BikeDetailsViewController.php';
 require_once 'controller/PaymentMethodViewController.php'; // Include your PaymentMethodViewController
 require_once 'controller/PayDepositViewController.php';
+require_once 'controller/ReturningDockViewController.php';
 require_once 'model/dock/DockManager.php'; // Include your DockManager file
 require_once 'model/bike/BikeManager.php'; // Include your BikeManager file
 
@@ -46,5 +47,10 @@ elseif ($request === 'processSession') {
     
     $sessionViewController = new SessionViewController();
     $sessionViewController->processSession();
+}
+elseif ($request === 'returningDock') {
+
+    $returningDockViewController = new ReturningDockViewController();
+    $returningDockViewController->displayReturningDockView();
 }
 ?>
